ec1590432c56e22b47ff47aa8c85c2f23bbb77fc
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-03 21:12:21 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/tiago/helpdesk/domain/Cliente.java
+++ src/main/java/com/tiago/helpdesk/domain/Cliente.java
@@ -6,13 +6,15 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tiago.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
-	@OneToMany(mappedBy = "tecnico")
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
--- src/main/java/com/tiago/helpdesk/domain/Tecnico.java
+++ src/main/java/com/tiago/helpdesk/domain/Tecnico.java
@@ -6,13 +6,15 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tiago.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Tecnico extends Pessoa {
 	private static final long serialVersionUID = 1L;
 
-	@OneToMany(mappedBy = "cliente")
+	@JsonIgnore
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
