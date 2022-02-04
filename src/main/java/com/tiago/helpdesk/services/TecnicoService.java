3bab4c481da3a4e3daa2c91182dfce6e70a8d376
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-03 22:00:51 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/tiago/helpdesk/services/TecnicoService.java
+++ src/main/java/com/tiago/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tiago.helpdesk.domain.Tecnico;
 import com.tiago.helpdesk.repositories.TecnicoRepository;
+import com.tiago.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
