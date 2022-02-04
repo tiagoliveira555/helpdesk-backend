b18cd8720c3520d5fff44861656e652026758f8f
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-03 22:20:43 -0300

    endpoing create para Tecnico

--- src/main/java/com/tiago/helpdesk/services/TecnicoService.java
+++ src/main/java/com/tiago/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tiago.helpdesk.domain.Tecnico;
+import com.tiago.helpdesk.domain.dtos.TecnicoDTO;
 import com.tiago.helpdesk.repositories.TecnicoRepository;
 import com.tiago.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class TecnicoService {
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+	}
+	
+	
 }
