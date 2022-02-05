703766f5d6eecc437370c244486f5dc203479176
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-05 18:47:33 -0300

    Autenticação com JWT

---
+++ src/main/java/com/tiago/helpdesk/services/UserDetailsServicImpl.java
@@ -0,0 +1,29 @@
+package com.tiago.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.tiago.helpdesk.domain.Pessoa;
+import com.tiago.helpdesk.repositories.PessoaRepository;
+import com.tiago.helpdesk.security.UserSS;
+
+@Service
+public class UserDetailsServicImpl implements UserDetailsService {
+	
+	@Autowired
+	private PessoaRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+}
