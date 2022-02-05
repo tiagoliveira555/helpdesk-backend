339c768c4ab05a756d28574f372f9d70bbf75b7f
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-04 21:12:47 -0300

    endpoint create para chamado criado

--- src/main/java/com/tiago/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/tiago/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.tiago.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tiago.helpdesk.domain.Chamado;
 
@@ -14,11 +16,23 @@ public class ChamadoDTO implements Serializable {
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	
+	@NotNull(message = "O Campo PRIORIDADE é requerido")
 	private Integer prioridade;
+	
+	@NotNull(message = "O Campo STATUS é requerido")
 	private Integer status;
+	
+	@NotNull(message = "O Campo TÍTULO é requerido")
 	private String titulo;
+	
+	@NotNull(message = "O Campo OBSERVAÇÕES é requerido")
 	private String observacoes;
+	
+	@NotNull(message = "O Campo TÉCNICO é requerido")
 	private Integer tecnico;
+	
+	@NotNull(message = "O Campo CLIENTE é requerido")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
