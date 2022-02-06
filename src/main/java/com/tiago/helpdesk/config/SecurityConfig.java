0291c1566162ba55376ae750847ae06319d0890d
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-06 13:07:00 -0300

    configurado erro no Cors

--- src/main/java/com/tiago/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/tiago/helpdesk/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Bean
-	CorsConfigurationSource corsConfiguration() {
+	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
