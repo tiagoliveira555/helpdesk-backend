afef24dca0cf606efd921d6002aaae3a367ab030
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-07 22:10:35 -0300

    adicionado Técnico teste

--- src/main/java/com/tiago/helpdesk/services/DBService.java
+++ src/main/java/com/tiago/helpdesk/services/DBService.java
@@ -27,7 +27,7 @@ public class DBService {
 
 	public void instanciaDB() {
 
-		Tecnico tec1 = new Tecnico(null, "Tiago Oliveira", "336.966.810-67", "tiago@mail.com", encoder.encode("123"));
+		Tecnico tec1 = new Tecnico(null, "Técnico Teste", "336.966.810-67", "teste@mail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
 		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", encoder.encode("123"));
 		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", encoder.encode("123"));
